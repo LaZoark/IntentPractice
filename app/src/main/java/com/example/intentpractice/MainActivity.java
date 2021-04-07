64c4c005a49884ba63db317a908707a058e1fd2a
LaZoark <emersonchen086@icloud.com>
2021-04-07 19:59:53 +0800

    work fine

--- app/src/main/java/com/example/intentpractice/MainActivity.java
+++ app/src/main/java/com/example/intentpractice/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.intentpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.DownloadManager;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,20 +15,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void quickActionView(View v){
+    public void universalQuickAction(View v){
         Intent it = new Intent();
-        it.setAction(Intent.ACTION_VIEW);
-        Uri uri = Uri.parse("http://google.com");
-        it.setData(uri);
-
-        startActivity(it);
-    }
-    public void quickActionWeb(View v){
-        Intent it = new Intent(Intent.ACTION_WEB_SEARCH);
-//        it.setAction();
-//        Uri uri = Uri.parse();
-//        it.setData(uri);
-        it.putExtra(SearchManager.QUERY,"android");    
-        startActivity(it);
+        switch (v.getId()){
+            case R.id.button_camera:
+                Toast.makeText(MainActivity.this,"Coming soon(Not really sure)",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button_email:
+                String[] cc = {"b0742006@ems.niu.edu.tw"};
+                String[] TO = {"b0742017@ems.niu.edu.tw"};
+                it.setType("text/plain");
+                it.setAction(Intent.ACTION_SENDTO);
+                it.setData(Uri.parse("mailto:"));
+                it.putExtra(Intent.EXTRA_EMAIL, TO);
+                it.putExtra(Intent.EXTRA_CC, cc);
+                it.putExtra(Intent.EXTRA_SUBJECT, "Testing from Android Xavier");
+                it.putExtra(Intent.EXTRA_TEXT, "What doth live");
+                break;
+            case R.id.button_google:
+                it.setAction(Intent.ACTION_VIEW);
+                Uri uri = Uri.parse("http://google.com");
+                it.setData(uri);
+                break;
+            case R.id.button_webSearch:
+                it.setAction(Intent.ACTION_WEB_SEARCH);
+                it.putExtra(SearchManager.QUERY, "android");
+                break;
+            case R.id.button_screenShot:
+                break;
+        }
+        if(v.getId()!=R.id.button_email&&v.getId()!=R.id.button_camera)
+            startActivity(it);
+        else {
+            startActivity(Intent.createChooser(it, "Send mail..."));
+//            finish();
+        }
     }
 }
